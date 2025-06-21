efd40cfbf32e292c25358629ccfa5c5d94f19483
Ferg <fergus.ampuan@gmail.com>
2025-06-21 21:40:13 +1200

    refactor: remove unused TypeAnimation import from Upload component

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,6 +1,5 @@
 import { React, useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { TypeAnimation } from "react-type-animation";
 import SwalModal from "../SwalModal/SwalModal.jsx";
 import PlantCard from "../PlantCard/PlantCard.jsx";
 
